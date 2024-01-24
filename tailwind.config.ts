38d51fc6d4531a77282c8c9b0ea609af487eae5f
Sara-MF <sara.melo@aluno.uece.br>
2024-01-24 15:55:00 -0300

    style: changing pattern file from png to svg

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        'food-pattern': "url('/pattern.png')"
+        'food-pattern': "url('/pattern.svg')"
       },
     },
   },
